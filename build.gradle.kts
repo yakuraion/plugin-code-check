1362b3671250ce694c60d425e6b06e332c292586
Vyacheslav Yakovlev <yakuraion@gmail.com>
2023-01-29 20:11:17 +0300

    update detekt to 1.22.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,5 +21,5 @@ publishing {
 
 dependencies {
     implementation("pro.yakuraion.plugins.utils:utils:1.0-SNAPSHOT")
-    implementation("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.20.0")
+    implementation("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.22.0")
 }
